4f12e3057d59632c63f150f7f0c3b2679026c32b
Ankit <ankitsharmaaa2003@gmail.com>
2024-04-13 16:37:40 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,55 @@
+const arr = [1, 2, 3];  // 1-Method
+// console.log(arr);
+
+const myarr = new Array("Ankit Sharma", "Prem Choudhary", "Tushar Nagar");  // 2-Method
+console.log(myarr);
+
+myarr[1] = "Amit";  // can update
+// console.log(myarr[1]);
+
+
+//method
+const myArr = new Array(0, 1, 2, 3, 4);
+// myArr.push("Ankit Sharma");  // add new Value
+// myArr.push(1);
+// myArr.pop();  // remove last value
+// myArr.unshift(2); // add staring postion
+// myArr.shift();  // remove starting postion
+// console.log(myArr);
+
+// console.log(myArr.includes(1)); // includes tell value is present or not like true & false
+// console.log(myArr.indexOf(4));  // tell the index 
+
+// const newArr = myArr.join();  // convert String format
+
+// const myn1 = myArr.splice(1, 3); // return value 1, 2, 3 in array
+// console.log(myn1);
+
+// const myn2 = myArr.splice()
+
+
+// const cars_name = ["Fortuner", "Range-Rover", "Volvo"];
+// const man_name = ["Ankit", "Hitesh", "Tushar"];
+
+// cars_name.push(man_name);
+// console.log(cars_name);
+
+// const all_new_heros = [...cars_name, ...man_name]; //spread operator
+// console.log(all_new_heros);
+
+
+
+// convert into Array
+// const scrore1 = 100;
+// const scrore2 = 200;
+// const scrore3 = 300;
+
+// const convertedArray = Array.of(scrore1, scrore2, scrore3);
+// console.log(convertedArray);
+
+
+
+
+
+
+
---
+++ lexical_closure.js
@@ -0,0 +1,15 @@
+
+function init() {
+    let name = "Mozilla";
+    function displayName() {
+        console.log(name);
+    }
+    displayName();
+}
+// init();
+
+function Outer() {
+    let username = "Ankit Sharma";
+}
+Outer();
+console.log(username);
