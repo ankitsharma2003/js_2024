9a51272fe663b49957463910c38ec73183ca65d7
Ankit <ankitsharmaaa2003@gmail.com>
2024-04-14 07:53:21 +0530

    datatype summary

--- lecture 1/04_datatypes_summary.js
+++ lecture 1/04_datatypes_summary.js
@@ -1,3 +1,39 @@
 // TYPES OF DATA TYPE
 
 // 1. Primitive Data type
+
+// 7 types : String, Number, Boolean, Undefined, Null, Sysmbol, BigInt
+
+let score = 89;  //Number
+let name = "Ankit Sharma";  //String
+
+let indianPeople = 192999393293929930n;  //BigInt
+
+const isLogged = true; //Boolean
+const outsideTemp = null;  //Null
+let userEmail;  //Undefined
+
+const id = Symbol('123'); //Sysmbol
+const anotherId = Symbol('123');  //Sysmbol
+
+// console.log(id === anotherId);
+
+
+// 2. Reference (Non - primitive)
+
+// Array, Objects, Functions
+
+
+const favCars = ["Range-Rover", "Fortuner", "Mercedes", 21]  // Array
+// console.log(favCars);
+
+const myObj = {   // objects
+    name : "Ankit Sharma",
+    age : 21
+}
+// console.log(myObj);
+
+const Addition = function(a) {   // functions
+       console.log(4+5);
+}
+// Addition();
