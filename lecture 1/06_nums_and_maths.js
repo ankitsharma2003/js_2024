68ee41b3d933f6063cbbd8fd53ba87a266f1d49d
Ankit <ankitsharmaaa2003@gmail.com>
2024-04-14 10:36:32 +0530

    nums & math funtion

---
+++ lecture 1/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+// **************** Numbers *****************
+
+const score = 200;    // this is number
+// console.log(score);
+
+const balance = new Number(100);  // this is also a number specify 
+// console.log(balance);
+
+
+// convert number into String
+// console.log(balance.toString());
+
+// toFixed method
+// console.log(balance.toFixed(2));
+
+//toLocaleString method => seprate , 
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));  // indian format
+
+
+
+
+
+//  ++++++++++++++++++++ MATHS +++++++++++++++++++++
+
+// console.log(Math.pow(2,2));  // power method
+// console.log(Math.abs(-4));   // convert negtive to postive
+// console.log(Math.round(12.34));  // round of value
+// console.log(Math.floor(4.9)); // lowest value get
+// console.log(Math.ceil(4.1));  // highest value get
+// console.log(Math.min(2,1,4,5));  // get lowest value
+// console.log(Math.max(2, 4, 1, 56)); // get higest value
+
+console.log(Math.random());  // give random numbers from 0 to 1
+console.log(Math.random()*10);
