a6bbdf59ee90e3ead2f3fd96423f62cc149f0ff5
Ankit <ankitsharmaaa2003@gmail.com>
2024-04-15 07:51:49 +0530

    learn dates in js

---
+++ lecture 1/07_dates.js
@@ -0,0 +1,17 @@
+// Dates
+
+const dates = new Date();
+// console.log(typeof dates);   // type => object
+
+// console.log(dates.toDateString());
+// console.log(dates.toLocaleString());
+
+
+let myCreatedDate = new Date(2024, 4, 12);
+// console.log(myCreatedDate.toDateString());
+
+
+// TIMESTAMP
+const myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));  // convert into seconds
